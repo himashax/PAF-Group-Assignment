c47f990ecf15bb112fe7e7d221c6313a730ae32e
malakadasun <greenpagelocal7@gmail.com>
2021-04-21 00:46:25 +0530

    crud operations

--- src/com/dao/PaymentDAOImpl.java
+++ src/com/dao/PaymentDAOImpl.java
@@ -1,6 +1,8 @@
 package com.dao;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.dbConnection.DBConnection;
 import com.model.Payment;
@@ -36,6 +38,96 @@ public class PaymentDAOImpl {
 	}
 	return result;
 	}
+
+	public List<Payment> listPayments(){
+
+		List<Payment> paymentList = new  ArrayList<>();
+		
+		try{
+			Connection connection = db.getConnection();
+			String payList = "select * from payment";
+			
+			Statement st = connection.createStatement();
+			ResultSet rs = st.executeQuery(payList);
+
+			while(rs.next()){
+				Payment payment = new Payment();
+
+				payment.setId(rs.getInt(1));
+				payment.setPaymentID(rs.getInt(2));
+				payment.setAmount(rs.getDouble(3));
+				payment.setType(rs.getString(4));
+				payment.setProductID(rs.getInt(5));
+				payment.setCustomerID(rs.getInt(6));
+
+				paymentList.add(payment);
+			}			
+
+		}catch(SQLException e){
+			e.printStackTrace();
+	 	} 
+		
+		
+		return paymentList;
+	}
+	public Payment getPaymentById(int id) {
+    	Payment p = new Payment();
+    	Connection connection = db.getConnection();
+    	String oId = "select * from payment where id = '"+id+"'";
+    	try {
+			Statement s = connection.createStatement();
+			ResultSet rs = s.executeQuery(oId);
+			while(rs.next()) {
+				p.setId(rs.getInt(1));
+				p.setPaymentID(rs.getInt(2));
+				p.setAmount(rs.getDouble(3));
+				p.setType(rs.getString(4));
+				p.setProductID(rs.getInt(5));
+				p.setCustomerID(rs.getInt(6));
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+    	return p;
+    	
+}
+	public String updatePayment(Payment payment) {
+		String output = "";
+		try {
+			Connection con = db.getConnection();
+			
+			String updatePayment = "UPDATE payment SET paymentID='"+payment.getPaymentID()+"',amount='"+payment.getAmount()+"',type='"+payment.getType()+"',productID='"+payment.getProductID()+"',`customerID`='"+payment.getCustomerID()+"' WHERE id ='"+payment.getId()+"'";
+			
+			PreparedStatement st = con.prepareStatement(updatePayment);
+			
+			st.executeUpdate();
+			output = "Update Sucessfull";
+			con.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return output;
+
+	}
+	public String delPayment(int id) {
+		String result ="";
+		try {
+		Connection connection = db.getConnection();
+		String delQry = "delete from payment where id = '"+id+"'";
+
+		PreparedStatement ps = connection.prepareStatement(delQry);
+		ps.execute();
+		
+		result = "Successfully deleted";
+		
+		connection.close();
+		} catch (SQLException e) {
+
+		e.printStackTrace();
+		}
+		return result;
+		}
 
 
 }
--- src/com/services/PaymentService.java
+++ src/com/services/PaymentService.java
@@ -1,8 +1,15 @@
 package com.services;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.dao.PaymentDAOImpl;
@@ -26,4 +33,31 @@ public class PaymentService {
 	}
 
 
+	@GET
+	@Path("/getPayment")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Payment> getAllPayment(){
+		return payDao.listPayments();
+	}
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+		public Payment getPayById(@PathParam("id") int id) {
+		return payDao.getPaymentById(id);
+	}
+	@PUT
+	@Path("/updatepayment") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updateProduct(Payment payment) 
+	{ 
+		return payDao.updatePayment(payment); 
+	}
+	@DELETE
+	@Path("/delPayment/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+		public String delPayment(@PathParam("id") int id) {
+		return payDao.delPayment(id);
+		
+	}
 }
