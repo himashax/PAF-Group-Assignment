ac588c4ff883a429366986caf56242ee56b47d79
Shalini2901 <dinurika.s99@gmail.com>
2021-04-20 09:34:37 +0530

    Comments changes in DBConnection

--- src/main/java/com/dbConnection/DBConnection.java
+++ src/main/java/com/dbConnection/DBConnection.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//A common method to connect to the DB
 public class DBConnection {
 	public Connection connect() {
 		Connection connection = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
+			//Adding necessary details to connect to the Database server
 			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_project", "root", "");
 			
 			if(connection != null) {
