b1aeecf6ef3b105b6e03eb04d14b7f38f169b645
sandamaligeethika <sgeethika98@gmail.com>
2021-04-23 22:45:52 +0530

    add comments to service class

--- src/com/services/ProductService.java
+++ src/com/services/ProductService.java
@@ -16,6 +16,7 @@ import com.models.Product;
 @Path("/product")
 public class ProductService {
 
+	//declare a common ProductDaoImpl type object 
 	private ProductDaoImpl pro = new ProductDaoImpl();
 	List<Product> products;
 
@@ -24,16 +25,23 @@ public class ProductService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN) 
 	public String createProduct(Product p1) {
+		//check the name field is empty or date is null or price is 0
 		if(p1.getName().isEmpty() || p1.getDate() == null || p1.getPrice()==0) {
 			return "Fill in the empty fields";
 		}
+		//check whether the new product Id is a duplicate Id 
 		if(pro.getProductById(p1.getProductId()).getId() == 0) {
+			//call the createProduct() method to create the new product 
 			pro.createProduct(p1);
+			//return a successful message
 			return "Inserted product " + p1.getProductId() + " successfully";
 		}else {
+			//return message if the product id is already existing
 			return "Already existing product id";
 		}		
 	}
+	
+	//get list of products 
 	
 	@GET
 	@Path("/productList")
@@ -42,6 +50,8 @@ public class ProductService {
 		return pro.listProducts();
 	}
 
+	//get a particular product by passing the productId
+	
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -49,26 +59,39 @@ public class ProductService {
 		return pro.getProductById(id);
 	}
 
+	
+	//update the product 
+	
 	@PUT
 	@Path("/update") 
 	@Consumes(MediaType.APPLICATION_JSON) 
 	@Produces(MediaType.TEXT_PLAIN) 
 	public String updateProduct(Product product) 
 	{ 
+		//check whether the name is empty or id is 0 when updating 
 		if (product.getName() == null || product.getName().isEmpty() || product.getId() == 0) {
+			
+			//display a message to enter the product name if the name field is empty.
 			return "Enter Product Name";
 		}else {
+			//update the product details
 			 pro.updateProduct(product); 
+			 
+			 //display update success message
 			 return "Updated "+ product.getProductId() +" product successfully";
 		}
 	
 	}
 	
+	//delete a particular product by passing the id
+	
 	@DELETE
 	@Path("/delproduct/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String deleteProduct(@PathParam("id") int id) {
+		//call the deleteProduct() to delete the product
 		pro.deleteProduct(id);
+		//confirm message 
 		return "Deleted successfully";
 	}
 }
